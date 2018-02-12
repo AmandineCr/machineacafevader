12a24a849837c74ab161ae944e3945bd6c95698e
Amandine CESAR <amandine.cesar@campus-numerique-in-the-alps.com>
2018-02-12 11:27:16 +0100

    new Eloquent relationship

--- app/Drink.php
+++ app/Drink.php
@@ -8,3 +8,22 @@ class Drink extends Model
 {
     protected $fillable = ['drinkName','price'];
 }
+
+class Ingredients extends Model
+{
+    function sales(){
+        hasMany('app/Sales');
+    }
+    function ingredients(){
+        belongsToMany('app/Sales')->withPivot('dose');
+    }
+}
+
+class Sales extends Model
+{
+    function drink(){
+        belongsTo('app/Drink');
+
+        )
+    }
+}
